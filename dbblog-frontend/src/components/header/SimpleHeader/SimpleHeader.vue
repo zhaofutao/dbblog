028bcf98e902566cea38b77d627a452b2d2ffe54
zhaofutao <zhaofutao@zhaofutao.com>
2020-08-14 17:38:34 +0800

    同步一些改动

--- dbblog-frontend/src/components/header/SimpleHeader/SimpleHeader.vue
+++ dbblog-frontend/src/components/header/SimpleHeader/SimpleHeader.vue
@@ -10,8 +10,8 @@
       <div id="header"  v-show="show">
     <router-link id="logo" to="/">
       <img src="../../../assets/logo.png">
-      <span class="title">DB's Blog</span>
-      <span class="motto">success belongs to the persevering</span>
+      <span class="title">赵付涛的笔记本</span>
+      <span class="motto">但行好事，莫问前程。</span>
     </router-link>
     <ul id="nav">
       <li>
--- dbblog-manage-frontend/static/config/index-dev.js
+++ dbblog-manage-frontend/static/config/index-dev.js
@@ -5,5 +5,5 @@
   window.SITE_CONFIG = {}
   // api接口请求地址
   // window.SITE_CONFIG['baseUrl'] = 'http://182.254.171.61:8080/dbBlog'
-  window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/dbblog'
+  window.SITE_CONFIG['baseUrl'] = 'http://119.45.203.229:8086/dbblog'
 })()
