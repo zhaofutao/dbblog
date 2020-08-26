47b91a5e58815dc13e537c21bef6bc4879029bae
赵付涛 <futao.zhao@ifchange.com>
2020-08-26 20:19:23 +0800

    删除笔记、阅读

--- dbblog-frontend/src/components/header/SimpleHeader/SimpleHeader.vue
+++ dbblog-frontend/src/components/header/SimpleHeader/SimpleHeader.vue
@@ -14,23 +14,6 @@
       <span class="motto"></span>
     </router-link>
     <ul id="nav">
-      <li>
-        <form id="search-form" action="/articles/search">
-      <span class="algolia-autocomplete" style="position: relative; display: inline-block; direction: ltr;">
-        <input
-        type="text" id="search-query-nav" class="search-query st-default-search-input aa-input" name="keywords" v-model="keywords" @keyup.enter="submit"
-        autocomplete="off" spellcheck="false" role="combobox" aria-autocomplete="list" aria-expanded="false"
-        aria-owns="algolia-autocomplete-listbox-0" dir="auto" style="position: relative; vertical-align: top;">
-        <pre
-        aria-hidden="true"
-        style="position: absolute; visibility: hidden; white-space: pre; font-family: system-ui; font-size: 12px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: normal; word-spacing: 0px; letter-spacing: normal; text-indent: 0px; text-rendering: auto; text-transform: none;"></pre>
-        <span
-        class="aa-dropdown-menu" role="listbox" id="algolia-autocomplete-listbox-0"
-        style="position: absolute; top: 100%; z-index: 100; display: none; left: 0px; right: auto;"><div
-        class="aa-dataset-1"></div></span></span>
-        </form>
-      </li>
-
       <li><a href="/articles" class="nav-link contribute">文章</a></li>
       <li><a href="/timeline" class="nav-link contribute">时光轴</a></li>
       <li><a href="/article/1" class="nav-link contribute">关于</a></li>
